4bd83a7515842741b7aa696ffb50e163ab325028
unknown <you@example.com>
2023-04-27 17:36:18 -0300

    completed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const tasks = [
+	{ id: 1, title: "comprar", completed: true },
+	{ id: 2, title: "gym", completed: false },
+	{ id: 3, title: "cocinar", completed: false },
+]
+
+function TaskList({ tasks }) {
+	return (
+		<ul>
+			{tasks?.map(task => (
+				<li key={task.id}>{task.title}</li>
+			))}
+		</ul>
+	)
 }
 
-export default App;
+function Filter({ children }) {
+	const [filterState, setFilterState] = useState("all")
+
+	const handleFilterChange = event =>
+		setFilterState(event.target.value)
+
+	return children(filterState, handleFilterChange)
+}
+
+function App() {
+	return (
+		<Filter>
+			{(filterState, handleFilterChange) => {
+				return (
+					<div>
+						<label htmlFor="filter">Filtrar pro estado: </label>
+						<select
+							id="filter"
+							value={filterState}
+							onChange={handleFilterChange}
+						>
+							<option value="all">Todos</option>
+							<option value="completed">Completados</option>
+							<option value="uncompleted">
+								Sin completar
+							</option>
+						</select>
+						<TaskList
+							tasks={
+								filterState === "all"
+									? tasks
+									: tasks.filter(task =>
+											filterState === "completed"
+												? task.completed
+												: !task.completed
+									  )
+							}
+						/>
+					</div>
+				)
+			}}
+		</Filter>
+	)
+}
+
+export default App
